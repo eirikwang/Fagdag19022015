e4ead942fd15f4cd3eba0f7a80832b8fce687f0d
Eirik Wang <eirilwan@gmail.com>
2015-02-19 14:39:12 +0100

    cleancode

--- src/MainPortal.java
+++ src/MainPortal.java
@@ -1,36 +1,31 @@
-import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.ListeningExecutorService;
-import com.google.common.util.concurrent.MoreExecutors;
-
-import java.util.Arrays;
 import java.util.concurrent.*;
 
 public class MainPortal {
     private final OppgaveHandterer oppgaveHandterer;
     private final NavnHandterer navnHandterer;
     ExecutorService executor = Executors.newFixedThreadPool(4);
-    CompletionService<Long> completionService = new ExecutorCompletionService<>(executor);
-
-    public MainPortal(OppgaveHandterer oppgaveHandterer, NavnHandterer navnHandterer){
+fjern
+    public MainPortal(OppgaveHandterer oppgaveHandterer, NavnHandterer navnHandterer) {
         this.oppgaveHandterer = oppgaveHandterer;
         this.navnHandterer = navnHandterer;
     }
+
     public CompletableFuture<String> assemble() throws InterruptedException {
         CompletableFuture<String> navn = navnHandterer.getName();
         CompletableFuture<Long> resultat = oppgaveHandterer.utforBeregning();
         CompletableFuture<Void> done = CompletableFuture.allOf(navn, resultat);
-        return done.thenApply((a) ->  "hei " + withTimeout(navn, "[Utilgjengelig]") + ". Result is: " + withTimeout(resultat, "[Utilgjengelig]" ));
+        return done.thenApply((a) -> "hei " + withTimeout(navn, "[Utilgjengelig]") + ". Result is: " + withTimeout(resultat, "[Utilgjengelig]"));
     }
 
     private String withTimeout(Future<?> navn, String defaultString) {
         try {
             return navn.get(400, TimeUnit.MILLISECONDS).toString();
-        } catch (Exception ignored) {}
+        } catch (Exception ignored) {
+        }
         return defaultString;
     }
 
-    public void shutdown(){
+    public void shutdown() {
         executor.shutdown();
     }
 }
